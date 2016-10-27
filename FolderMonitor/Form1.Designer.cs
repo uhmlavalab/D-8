6379e800a9994842b6852be9e00aea2201debde7
Jack Lam <jacksylam@gmail.com>
2016-10-26 20:41:40 -1000

    Made UI more symmterical and balanced.

--- FolderMonitor/Form1.Designer.cs
+++ FolderMonitor/Form1.Designer.cs
@@ -50,10 +50,9 @@
             // 
             this.UploadBtn.BackColor = System.Drawing.Color.SkyBlue;
             this.UploadBtn.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UploadBtn.Location = new System.Drawing.Point(112, 299);
-            this.UploadBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.UploadBtn.Location = new System.Drawing.Point(89, 316);
             this.UploadBtn.Name = "UploadBtn";
-            this.UploadBtn.Size = new System.Drawing.Size(230, 75);
+            this.UploadBtn.Size = new System.Drawing.Size(172, 61);
             this.UploadBtn.TabIndex = 1;
             this.UploadBtn.Text = "Upload";
             this.UploadBtn.UseVisualStyleBackColor = false;
@@ -62,9 +61,10 @@
             // StatusFile
             // 
             this.StatusFile.Font = new System.Drawing.Font("Arial", 18F);
-            this.StatusFile.Location = new System.Drawing.Point(10, 520);
+            this.StatusFile.Location = new System.Drawing.Point(10, 554);
+            this.StatusFile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.StatusFile.Name = "StatusFile";
-            this.StatusFile.Size = new System.Drawing.Size(456, 47);
+            this.StatusFile.Size = new System.Drawing.Size(342, 38);
             this.StatusFile.TabIndex = 1;
             this.StatusFile.Text = "Ready to Upload";
             this.StatusFile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -72,11 +72,11 @@
             // FileListBox
             // 
             this.FileListBox.Font = new System.Drawing.Font("Arial", 8F);
-            this.FileListBox.Location = new System.Drawing.Point(29, 232);
-            this.FileListBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FileListBox.Location = new System.Drawing.Point(28, 261);
+            this.FileListBox.Margin = new System.Windows.Forms.Padding(2);
             this.FileListBox.Multiline = true;
             this.FileListBox.Name = "FileListBox";
-            this.FileListBox.Size = new System.Drawing.Size(450, 60);
+            this.FileListBox.Size = new System.Drawing.Size(307, 50);
             this.FileListBox.TabIndex = 5;
             this.FileListBox.TextChanged += new System.EventHandler(this.FileListBox_TextChanged);
             // 
@@ -88,10 +88,9 @@
             // 
             this.browsefolderBtn.BackColor = System.Drawing.Color.SkyBlue;
             this.browsefolderBtn.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.browsefolderBtn.Location = new System.Drawing.Point(112, 152);
-            this.browsefolderBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.browsefolderBtn.Location = new System.Drawing.Point(89, 176);
             this.browsefolderBtn.Name = "browsefolderBtn";
-            this.browsefolderBtn.Size = new System.Drawing.Size(230, 75);
+            this.browsefolderBtn.Size = new System.Drawing.Size(172, 61);
             this.browsefolderBtn.TabIndex = 0;
             this.browsefolderBtn.Text = "Select Folder";
             this.browsefolderBtn.UseVisualStyleBackColor = false;
@@ -101,10 +100,9 @@
             // 
             this.CleanDirBtn.BackColor = System.Drawing.Color.SkyBlue;
             this.CleanDirBtn.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CleanDirBtn.Location = new System.Drawing.Point(626, 299);
-            this.CleanDirBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.CleanDirBtn.Location = new System.Drawing.Point(485, 315);
             this.CleanDirBtn.Name = "CleanDirBtn";
-            this.CleanDirBtn.Size = new System.Drawing.Size(230, 75);
+            this.CleanDirBtn.Size = new System.Drawing.Size(172, 61);
             this.CleanDirBtn.TabIndex = 3;
             this.CleanDirBtn.Text = "Delete";
             this.CleanDirBtn.UseVisualStyleBackColor = false;
@@ -114,9 +112,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 24F);
-            this.label3.Location = new System.Drawing.Point(162, 72);
+            this.label3.Location = new System.Drawing.Point(121, 88);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 45);
+            this.label3.Size = new System.Drawing.Size(113, 36);
             this.label3.TabIndex = 9;
             this.label3.Text = "Upload";
             // 
@@ -124,9 +123,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 24F);
-            this.label4.Location = new System.Drawing.Point(682, 72);
+            this.label4.Location = new System.Drawing.Point(519, 85);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(134, 45);
+            this.label4.Size = new System.Drawing.Size(105, 36);
             this.label4.TabIndex = 10;
             this.label4.Text = "Delete";
             // 
@@ -134,49 +134,51 @@
             // 
             this.browseDeleteButton.BackColor = System.Drawing.Color.SkyBlue;
             this.browseDeleteButton.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.browseDeleteButton.Location = new System.Drawing.Point(626, 152);
-            this.browseDeleteButton.Margin = new System.Windows.Forms.Padding(4);
+            this.browseDeleteButton.Location = new System.Drawing.Point(485, 176);
             this.browseDeleteButton.Name = "browseDeleteButton";
-            this.browseDeleteButton.Size = new System.Drawing.Size(230, 75);
+            this.browseDeleteButton.Size = new System.Drawing.Size(172, 61);
             this.browseDeleteButton.TabIndex = 2;
-            this.browseDeleteButton.Text = "Select Folder from Z:\\Kanaloa1";
+            this.browseDeleteButton.Text = "Select Folder from Kanaloa1";
             this.browseDeleteButton.UseVisualStyleBackColor = false;
             this.browseDeleteButton.Click += new System.EventHandler(this.browseDeleteButton_Click);
             // 
             // label5
             // 
             this.label5.BackColor = System.Drawing.Color.SkyBlue;
-            this.label5.Location = new System.Drawing.Point(500, 84);
+            this.label5.Location = new System.Drawing.Point(373, 95);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(5, 444);
+            this.label5.Size = new System.Drawing.Size(10, 436);
             this.label5.TabIndex = 12;
             // 
             // FileDeleteBox
             // 
             this.FileDeleteBox.Font = new System.Drawing.Font("Arial", 8F);
-            this.FileDeleteBox.Location = new System.Drawing.Point(530, 233);
-            this.FileDeleteBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FileDeleteBox.Location = new System.Drawing.Point(423, 261);
+            this.FileDeleteBox.Margin = new System.Windows.Forms.Padding(2);
             this.FileDeleteBox.Multiline = true;
             this.FileDeleteBox.Name = "FileDeleteBox";
-            this.FileDeleteBox.Size = new System.Drawing.Size(450, 60);
+            this.FileDeleteBox.Size = new System.Drawing.Size(307, 50);
             this.FileDeleteBox.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(278, 9);
+            this.label1.Font = new System.Drawing.Font("Arial", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(470, 63);
+            this.label1.Size = new System.Drawing.Size(353, 49);
             this.label1.TabIndex = 14;
             this.label1.Text = "Destiny Uploader";
             // 
             // StatusDelete
             // 
             this.StatusDelete.Font = new System.Drawing.Font("Arial", 18F);
-            this.StatusDelete.Location = new System.Drawing.Point(533, 525);
+            this.StatusDelete.Location = new System.Drawing.Point(419, 554);
+            this.StatusDelete.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.StatusDelete.Name = "StatusDelete";
-            this.StatusDelete.Size = new System.Drawing.Size(416, 47);
+            this.StatusDelete.Size = new System.Drawing.Size(312, 38);
             this.StatusDelete.TabIndex = 15;
             this.StatusDelete.Text = "Ready to Delete";
             this.StatusDelete.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -187,9 +189,10 @@
             this.pictureBox1.Image = global::CCDestinyUploader.Properties.Resources.LAVAlogoreisze3;
             this.pictureBox1.ImageLocation = "";
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(421, 583);
+            this.pictureBox1.Location = new System.Drawing.Point(300, 617);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(170, 126);
+            this.pictureBox1.Size = new System.Drawing.Size(160, 102);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
@@ -198,9 +201,10 @@
             this.pictureBox2.ErrorImage = null;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.InitialImage = null;
-            this.pictureBox2.Location = new System.Drawing.Point(85, 122);
+            this.pictureBox2.Location = new System.Drawing.Point(28, 126);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(288, 408);
+            this.pictureBox2.Size = new System.Drawing.Size(307, 405);
             this.pictureBox2.TabIndex = 16;
             this.pictureBox2.TabStop = false;
             // 
@@ -209,17 +213,18 @@
             this.pictureBox3.ErrorImage = null;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
             this.pictureBox3.InitialImage = null;
-            this.pictureBox3.Location = new System.Drawing.Point(592, 122);
+            this.pictureBox3.Location = new System.Drawing.Point(423, 126);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(288, 408);
+            this.pictureBox3.Size = new System.Drawing.Size(308, 405);
             this.pictureBox3.TabIndex = 17;
             this.pictureBox3.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.ClientSize = new System.Drawing.Size(765, 730);
             this.Controls.Add(this.StatusDelete);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.FileDeleteBox);
@@ -236,7 +241,6 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox3);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "CCDestinyUploader";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
